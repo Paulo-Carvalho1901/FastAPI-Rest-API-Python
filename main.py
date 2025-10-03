560b19e0c47d3b237bd64163b6af309e793e12ab
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-03 14:28:41 -0300

    feito comentarios em main.py e criado arquivo de rota de orders

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 # Para rodar nosso código, rodar no terminal - uvicorn main:app --reload
+
+# endpoint:
+# /ordens
+
+# REST APIs
+# GET -> LEITURA/PEGAR
+# POST -> ENVIAR/CRIAR
+# PUT/PET -> EDIÇÃO
+# DELETE -> DELETAR
