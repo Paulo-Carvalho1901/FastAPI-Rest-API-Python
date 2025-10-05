80733a53bbf4fe3681c96df27be20e42d671b13e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-05 01:47:30 -0300

    criando logica na estrutura para se criar um usuário de autorização

--- auth_routers.py
+++ auth_routers.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 
+from models import Usuario, db
+
+from sqlalchemy.orm import sessionmaker
+
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
 @auth_router.get('/')
@@ -8,3 +12,17 @@ async def autenticar():
     Éssa é a rota padrão de autenticação do nosso sistema
     """
     return {'mensagem': 'Você acessou a rota padrão de autenticação', 'autenticação': False}
+
+@auth_router.post('/criar_conta')
+async def criar_conta(email: str, senha: str, nome: str):
+    session = sessionmaker(bind=db)
+    session = session()
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if usuario:
+        # Já existe um usuário com esse email
+        return {'mensagem': 'Já existe um usuário com esse email.'}
+    else:
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {'mesagem': 'Usuário cadastrado com sucesso.'}
