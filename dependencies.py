f26362f4679d0eacfd0b2efcd2f3429b6e1d434e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-05 02:09:31 -0300

    depends do projeto

--- dependencies.py
+++ dependencies.py
@@ -2,9 +2,10 @@ from models import db
 from sqlalchemy.orm import sessionmaker
 
 
-
 def pegar_sessao():
-    session = sessionmaker(bind=db)
-    session = session()
-
-    return session
+    try:
+        session = sessionmaker(bind=db)
+        session = session()
+        yield session
+    finally:
+        session.close()
